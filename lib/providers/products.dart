fbf501d653a8b3619b267f00474d59b4571a5467
c-tsuyoki <cristian175.contato@gmail.com>
2021-04-09 18:04:41 -0300

    Add app files and verb post

---
+++ lib/providers/products.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+import 'dart:core';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import './product.dart';
+import '../data/dummy_data.dart';
+
+class Products with ChangeNotifier {
+  List<Product> _items = DUMMY_PRODUCTS;
+
+  List<Product> get items => [..._items];
+
+  int get itemsCount {
+    return _items.length;
+  }
+
+  List<Product> get favoriteItems {
+    return _items.where((prod) => prod.isFavorite).toList();
+  }
+
+  void addProduct(Product newProduct) {
+    const url =
+        'https://flutter-cod3r-bb57a-default-rtdb.firebaseio.com/products.json';
+
+    http.post(
+      url,
+      body: json.encode({
+        'title': newProduct.title,
+        'description': newProduct.description,
+        'price': newProduct.price,
+        'imageUrl': newProduct.imageUrl,
+        'isFavorite': newProduct.isFavorite,
+      }),
+    );
+
+    _items.add(Product(
+        id: Random().nextDouble().toString(),
+        title: newProduct.title,
+        description: newProduct.description,
+        price: newProduct.price,
+        imageUrl: newProduct.imageUrl));
+    notifyListeners();
+  }
+
+  void updateProduct(Product product) {
+    if (product == null || product.id == null) {
+      return;
+    }
+
+    final index = _items.indexWhere((prod) => prod.id == product.id);
+    if (index >= 0) {
+      _items[index] = product;
+      notifyListeners();
+    }
+  }
+
+  void deleteProduct(String id) {
+    final index = _items.indexWhere((prod) => prod.id == id);
+    if (index >= 0) {
+      _items.removeWhere((prod) => prod.id == id);
+      notifyListeners();
+    }
+  }
+}
+
+  // bool _showFavoriteOnly = false;
+  
+  // void showFavoriteOnly() {
+  //   _showFavoriteOnly = true;
+  //   notifyListeners();
+  
+  // }
+  // void showAll() {
+  //   _showFavoriteOnly = false;
+  //   notifyListeners();
+  // }
